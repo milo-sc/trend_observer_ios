3cc1d1414232ddfa3bf68ed3a7dd284f0ad31d02
Victor Wu <vicwu.off@gmail.com>
2024-07-02 17:17:31 -0400

    cleans up more extraneous code

--- trend_observer/ContentView.swift
+++ trend_observer/ContentView.swift
@@ -52,34 +52,6 @@ struct ContentView: View {
     }
 }
 
-//struct SearchView: View {
-//    @State private var stock: String = ""
-//    
-//    var body: some View {
-//        NavigationView {
-//            VStack {
-//                TextField("Enter a Stock", text: $stock)
-//                    .padding()
-//                    .background(Color(.systemGray6))
-//                    .cornerRadius(5.0)
-//                    .padding(.horizontal, 20)
-//                    .padding(.bottom, 20)
-//                
-//                NavigationLink(destination: DetailView()) {
-//                    Text("Check Price")
-//                        .font(.headline)
-//                        .padding()
-//                        .background(Color.black)
-//                        .foregroundColor(.white)
-//                        .cornerRadius(10)
-//                }
-//                .padding()
-//            }
-//            .navigationTitle("Trend Observer")
-//        }
-//    }
-//}
-
 struct DetailView: View {
     var viewModel: StockViewModel
     var symbol: String
@@ -89,8 +61,6 @@ struct DetailView: View {
             Text("Stock Price History")
                 .font(.largeTitle)
                 .padding()
-            
-            print(symbol)
             
             Button(action: {
                 viewModel.fetchStock(symbol: symbol)
